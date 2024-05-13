deb35b8df51256dcb8b03e48b9b5dfc5732f0927
afablelorenz <afablelorenz@gmail.com>
2024-05-13 21:18:34 +0200

    finished js of gioco dei dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,5 +1,14 @@
 document.querySelector('#giocare').addEventListener('click',function(){
     const numUtente=Math.floor(Math.random()*6)+1;
     const numComputer=Math.floor(Math.random()*6)+1;
-    if(numUtente)
+    if(numUtente>numComputer){
+        alert('Hai vinto!');
+        console.log(numUtente,numComputer);
+    }else if(numUtente<numComputer){
+        alert('Hai perso!');
+        console.log(numUtente,numComputer);
+    }else{
+        alert('Pareggio!');
+        console.log(numUtente,numComputer);
+    }
 });
