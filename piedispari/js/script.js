af171148fc441d746cc32afa077ad386da1973aa
afablelorenz <afablelorenz@gmail.com>
2024-05-13 23:12:50 +0200

    updated js script

--- piedispari/js/script.js
+++ piedispari/js/script.js
@@ -1,1 +1,13 @@
 let arrayVuoto = [];
+
+for(let i=0;i<6;i++){
+    let numUtente;
+    numUtente=Number.parseInt(prompt('Type a number'));
+    if(numUtente % 2 == 0){
+        arrayVuoto.push(numUtente);
+    }else{
+        console.log('odd number detected: '+numUtente);
+    }
+    
+}
+console.log(arrayVuoto);
