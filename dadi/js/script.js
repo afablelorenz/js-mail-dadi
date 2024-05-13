f2e73f7e5954fe21f6069d6aa411e0c36f0a7ad7
afablelorenz <afablelorenz@gmail.com>
2024-05-13 21:02:40 +0200

    added html elements and js script variables for gioco dei dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('#giocare').addEventListener('click',function(){
+    const numUtente=Math.floor(Math.random()*6)+1;
+    const numComputer=Math.floor(Math.random()*6)+1;
+    if(numUtente)
+});
