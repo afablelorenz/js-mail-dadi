a487c52e2c9051eeb1e4b2db3220afdf5e32924a
afablelorenz <afablelorenz@gmail.com>
2024-05-13 22:45:22 +0200

    updated js script

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,8 @@
 const email = ['qwe@gmail.com','asd@gmail.com','zxc@gmail.com','ewq@gmail.com','das@gmail.com','czx@gmail.com'];
+let userEmail = prompt('Inserire l\'email');
+
+if(email.includes(userEmail)){
+    alert('Sei nella lista!');
+}else{
+    alert('Non sei nella lista!');
+}
